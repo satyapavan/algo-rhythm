9281eefa6138d1a946c58f753a8767f5b357220a
Satya Pavan <pavan.simham@gmail.com>
2017-12-11 23:40:19 +0530

    a little more implementation

--- courses/deque/Deque.java
+++ courses/deque/Deque.java
@@ -1,5 +1,8 @@
 import java.util.Iterator;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 
 public class Deque<Item> implements Iterable<Item> {
 
@@ -27,7 +30,7 @@ public class Deque<Item> implements Iterable<Item> {
 	public boolean isEmpty()                 // is the deque empty?
 	{
 		if(ENABLE_LOG) System.out.println("isEmpty = " + (qSize == 0));
-		
+
 		return (qSize == 0);
 	}
 
@@ -41,7 +44,7 @@ public class Deque<Item> implements Iterable<Item> {
 	public void addFirst(Item item)          // add the item to the front
 	{
 		Node oldHead ;
-		
+
 		if(isEmpty())
 			oldHead = null;
 		else
@@ -50,23 +53,23 @@ public class Deque<Item> implements Iterable<Item> {
 		head.item = item;
 		head.next = oldHead;
 		head.prev = null;
-		
+
 		qSize++;
 	}
 
 	public void addLast(Item item)           // add the item to the end
 	{
 		Node oldTail;
-		
+
 		if(isEmpty())
 			oldTail = null;
 		else
 			oldTail = tail;
-		
+
 		tail.item = item;
 		tail.next = null;
 		oldTail.next = tail;
-		
+
 		qSize++;
 	}
 
@@ -76,33 +79,60 @@ public class Deque<Item> implements Iterable<Item> {
 
 		head = head.next;
 		head.prev = null;
-		
+
 		qSize--;
-		
+
 		return obj;
 	}
 
 	public Item removeLast()                 // remove and return the item from the end
 	{
 		Item obj = tail.item;
-		
+
 		tail = tail.prev;
 		tail.next = null;
-				
+
 		qSize--;
-		
+
 		return obj;
 	}
 
-	/*
 	public Iterator<Item> iterator()         // return an iterator over items in order from front to end
 	{
-
+		return new ListIterator();
 	}
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	private class ListIterator implements Iterator<Item>
+	{
+		private Node current = head;
+
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Item next() {
+
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+		
+        public void remove()      { throw new UnsupportedOperationException();  }
+	}
+
+
+	public static void main(String[] args) {
+		Deque<String> obj = new Deque<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+            	obj.addFirst(item);
+            else if (!obj.isEmpty())
+                StdOut.print(obj.removeLast() + " ");
+        }
+        StdOut.println("(" + obj.size() + " left on queue)");
 	}
 
 }
